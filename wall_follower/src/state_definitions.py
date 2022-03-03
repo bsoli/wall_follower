379f67c6311540d433e30e81a35adec80f4a98ff
bsoli <benjamintsoli@gmail.com>
2022-03-03 23:41:56 +0000

    Add files via upload

---
+++ wall_follower/src/state_definitions.py
@@ -0,0 +1,10 @@
+#Here is where you can define all of your states in a clever way to make your code more readable.
+#Think of all the states your robot could be in if thrown randomly into a world.
+#Currently the code is set up for a variable name being equal to an integer like:
+
+FOUND = 1
+FOLLOWING = 2
+LOOKING = 3
+
+#for each of your states.
+
